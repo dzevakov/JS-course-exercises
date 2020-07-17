168abaf7f5ad69e47e3123efbf503409c59ed2a4
dzevakov <dmitryzev@gmail.com>
2020-07-18 01:46:52 +0300

    Lesson of DataTypes

---
+++ Lessons/DataTypes/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let number = 5;
+const leftBorderWigth = 1;
+
+number = 10;
+console.log (number);
+
+const object = {
+    a: 50
+};
+
+object.a = 10;
+
+console.log (object);
+
+console.log (name);
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+
---
+++ Lessons/DataTypes/script007.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* const answers = [];
+
+answers[0] = promt("Ваш любимый цвет?");
+answers[1] = promt("Ваш любимый авто?");
+answers[2] = promt("Ваше любимое число?");
+
+document.write (answers); */
+
+const answer = promt ("How old are you?");
+console.log (answer);
