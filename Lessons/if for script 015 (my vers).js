c635c976dba09d364142033239ee12fcef11ddaa
dzevakov <dmitryzev@gmail.com>
2020-07-18 19:27:46 +0300

    added " if " and " for "

---
+++ Lessons/if for script 015 (my vers).js
@@ -0,0 +1,60 @@
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+let numberOfFilms;
+
+do {
+    numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
+}
+while (numberOfFilms == '' || numberOfFilms == null || numberOfFilms.length < 50);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {}, 
+    genres: [], 
+    privat: false 
+};
+
+if (personalMovieDB.count < 10) {
+    alert ("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert ("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert ("Вы киноман");
+} else {
+    alert ("Произошла ошибка");
+}
+
+let film, rateFilm;
+
+for (let i = 0; i < 2; i++) {
+    do {
+        film = prompt ('Один из последних просмотренных фильмов?', '');
+    }
+    while (film == '' || film == null || film.length < 50);
+
+    do {
+    rateFilm = prompt ('На сколько оцените его?', '');
+    }    
+    while (rateFilm == '' || rateFilm == null || rateFilm.length < 50);
+
+    personalMovieDB.movies [film] = rateFilm; 
+}
+    
+
+console.log (personalMovieDB);
+// Код возьмите из предыдущего домашнего задания
