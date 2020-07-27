f9bb105324e250f6e2df6addc91ae1452a59b086
dzevakov <dmitryzev@gmail.com>
2020-07-27 09:07:26 +0300

    Recursion Solution (homework)

--- Homework/2) recursion/recursion.js
+++ Homework/2) recursion/recursion.js
@@ -3,27 +3,27 @@
 const testObject1 = {
     name: 'aaa',
     id: 1
-}
+};
 
 const testObject2 = {
     name: 'bbb',
     id: 2,
     friend: testObject1
-}
+};
 
 const testObject3 = {
     name: 'ccc',
     id: 3,
     friend: testObject1,
     children: [ 'sss', 'bb', 22]
-}
+};
 
 const testObject4 = {
     name: 'ddd',
     id: 4,
     friend: testObject1,
     children: [ testObject1, '33', 555]
-}
+};
 
 const testObject5 = {
     allOf: [
@@ -79,16 +79,34 @@ const testObject5 = {
         ]
       }
     ]
-}
+};
 
 
 //Рекурсивная функция которая прходит по всем ключам обьекта и выводит их
 //использовать Object.keys(input), foreach, рекурсию
 //Нагуглить как проверить является ли переменная обектом, массивом, т.к. они будут по разному обрабатываться
+
+let space = '';
+
 const writeObject = (input) => {
-    Object.keys(input).forEach(key => {
+  Object.keys(input).forEach(key => {
+       
+    if (input[key] instanceof Object === true) {
+      space = space + '_';
+      writeObject(input[key]);
+      space = space.slice(0, -1);
+      return;
+    }
 
-    })
-} 
+    if (input[key] instanceof Object === false &&
+      input instanceof Array === false) {
+      console.log(`${space} ${key} - ${input[key]}`);
+    }
+    if(input instanceof Array === true) {
+      console.log(`${space} ${input[key]}`);
+    }
+  }
+  );
+};
 
-writeObject(testObject3)
+writeObject(testObject4);
