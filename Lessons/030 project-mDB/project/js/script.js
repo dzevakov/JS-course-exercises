8de4e7b5e6f6c060f9bf608fc85b79de89363b1c
dzevakov <dmitryzev@gmail.com>
2020-08-07 23:28:49 +0300

    Lesson 033 of practice JS basics _part 2

--- Lessons/030 project-mDB/project/js/script.js
+++ Lessons/030 project-mDB/project/js/script.js
@@ -14,51 +14,8 @@
 
 'use strict';
 
-// const bg = document.getElementsByClassName('promo__bg');
-// bg[0].style.backgroundImage = "url('../img/bg.jpg')";
-
-//get list elements.
-const list = document.getElementsByClassName('promo__interactive-item'); 
-
-// conver to normal array.
-const filmList = [];
-Object.keys(list).forEach( key => {
-    filmList.push(list[key].textContent);
-});
-
-// sort array
-filmList.sort();
-
-//write sorted data to list on HTML page
-Object.keys(list).forEach(key => {
-   list[key].textContent = filmList[key];
-});
-
-// let element = document.getElementsByClassName('promo__interactive-list');
-// console.log(element[0].tagName);
-
-// const attrib = element[0].attributes;
-// console.log(attrib.length);
-
-// const elementNew = document.createElement('ol');
-// element[0].parentElement.insertBefore(elementNew, element[0]);
-// elementNew.classList = element[0].classList;
-
-// const childNodesE = element[0].childNodes;
-// Object.keys(childNodesE).forEach(item => {
-//     elementNew.appendChild(childNodes[item]);
-// });
-
-// for (let i = 0; i < element[0].attributes.lengtht; ++i) {
-//     elementNew.setAttribute(element[0].attributes[i].name, element[0].attributes[i].value);
-// }
-
-
-// element[0].parentElement.removeChild(element[0]);
-
-// element = document.getElementsByClassName('promo__interactive-list');
-// console.log(element[0].tagName);
-
+const bg = document.getElementsByClassName('promo__bg');
+bg[0].style.backgroundImage = "url('../project/img/bg.jpg')";
 
 const movieDB = {
     movies: [
@@ -70,4 +27,60 @@ const movieDB = {
     ]
 };
 
+const sortFilmList = (movieDataBase) => {
+    //get list elements.
+    const list = document.getElementsByClassName('promo__interactive-item'); 
 
+    // sort array & write sorted data to list on HTML page
+    movieDataBase.movies.sort();
+    for (let i= 0; i < movieDB.movies.length; i++) {
+        list[i].innerHTML = `${i+1}. ${movieDB.movies[i]} \n <div class="delete"></div>`;
+    }
+};
+
+const addFilm = (filmNew) => {
+    if (filmNew.length >= 21) {
+        filmNew = `${filmNew.slice(0, 21)} ...`;
+    }
+
+    movieDB.movies.push(filmNew);
+   
+    const filmNewTag = document.createElement('li');
+    filmNewTag.innerHTML = `${filmNew} \n <div class="delete"></div>`;
+    filmNewTag.className = 'promo__interactive-item';
+   
+    const filmList = document.getElementsByClassName('promo__interactive-list');
+    filmList[0].append(filmNewTag);
+   
+    sortFilmList(movieDB);
+};
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const addInput = document.querySelector('.adding__input');
+    const filmNew = addInput.value;
+    addFilm(filmNew);
+
+    const check = document.getElementsByTagName('input');
+    if (check[2].checked) {
+        console.log('Добавляем любимый фильм');
+        alert('Добавляем любимый фильм');
+    }
+
+    addInput.value = '';
+    check[2].checked = false;
+});
+
+const deleteFilm = document.getElementsByClassName('delete');
+Object.keys(deleteFilm).forEach(key => {
+    deleteFilm[key].addEventListener('click', (event) => {
+       
+        movieDB.movies.splice(key, 1);
+        console.log(movieDB.movies);
+        
+        event.target.closest('li').remove();
+
+        sortFilmList(movieDB);
+    });
+});
