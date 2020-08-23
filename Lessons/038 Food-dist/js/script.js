e55e62e1be03da84d1e3cd6e33eb29160af741a7
dzevakov <dmitryzev@gmail.com>
2020-08-23 21:07:46 +0300

    course Java script in work

---
+++ Lessons/038 Food-dist/js/script.js
@@ -0,0 +1,195 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+// Add Tabs
+
+const tabs = document.querySelectorAll('.tabheader__item'),
+      tabsContent = document.querySelectorAll('.tabcontent'),
+      tabsParent = document.querySelector('.tabheader__items');
+
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        item.style.display = 'none';
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    });
+}
+
+function showTabContent(i = 0) {
+    tabsContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target && target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+
+// Timer
+
+const deadLine = '2020-08-24';
+
+function getTimeRemain(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date()),
+          days = Math.floor(t / (1000 * 60 * 60 * 24)),
+          hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+          minutes = Math.floor((t / (1000 * 60) % 60)),
+          seconds = Math.floor((t / (1000) % 60));
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return '0${num}';
+    } else {
+        return num;
+    }
+}
+
+function setClock(selector, endTime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemain(endTime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.timer', deadLine);
+
+// Modal window
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+      modal = document.querySelector('.modal'),
+      modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = 'hidden';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) =>{
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
+
+// Используем классы для карточек
+
+class MenuCard{
+    constructor(src, alt, title, descr, price, parentSelector) {
+        this.scr = src;
+        this.alt = alt;
+        this.title = title;
+        this.descr = descr;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.course = 3;
+        this.excange();
+    }
+
+    excange() {
+        this.price = this.price * this.course;
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>`;
+        this.parent.append(element);
+    }
+
+}
+
+  // const div = new MenuCard();
+    // div.render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        12,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        17,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        8,
+        ".menu .container"
+    ).render();
+
+});
