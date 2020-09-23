60425b4d3743145a7f70a9394745590629a50e15
Nikolai <n.darinsky@gmail.com>
2020-09-23 23:10:11 +0300

    Sample server

---
+++ Homework/Server/server.js
@@ -0,0 +1,26 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('Get Hello World!')
+})
+
+app.post('/', function (req, res, next) {
+    console.log(req.body)
+    res.json(req.body)
+  })
+
+app.post('/dar', function (req, res, next) {
+  console.log(req.body)
+  res.json(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
